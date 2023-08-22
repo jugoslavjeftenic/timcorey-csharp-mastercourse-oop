572fb9416b98ea437489a49fbe74b73371ffef98
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-22 11:00:44 +0200

    HomeworkPropertyTypes

---
+++ T17_01_02_HomeworkPropertyTypes/AddressModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace T17_01_02_HomeworkPropertyTypes
+{
+    public class AddressModel
+    {
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+
+        private string? _fullAddress;
+
+        public string FullAddress
+        {
+            get
+            {
+                string output = $"{Street} {City} {State} {Zip}";
+                return output;
+            }
+        }
+    }
+}
---
+++ T17_01_02_HomeworkPropertyTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace T17_01_02_HomeworkPropertyTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a class that has properties for street address, city, state, and zip code.
+            // Then add a FullAddress property that is read-only.
+
+            AddressModel address = new()
+            {
+                Street = "1st Street",
+                City = "Sweethaven Village",
+                State = "Malta",
+                Zip = "MLH 356",
+            };
+
+            Console.WriteLine(address.FullAddress);
+        }
+    }
+}
