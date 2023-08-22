760df565da234f97b92f4757e3048f2502f57a95
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-22 15:53:04 +0200

    HomeworkClassLibraries

---
+++ T18_01_02_HomeworkClassLibraries/Program.cs
@@ -0,0 +1,18 @@
+﻿using T18_01_02_HomeworkClassLibrariesLibrary.Models;
+
+namespace T18_01_02_HomeworkClassLibraries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a class library that holds a Person class.
+            // Use that class in a Console application.
+
+            PersonModel person = new();
+            person.Name = "Popeye";
+
+            Console.WriteLine(person.Name);
+        }
+    }
+}
---
+++ T18_01_02_HomeworkClassLibrariesLibrary/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace T18_01_02_HomeworkClassLibrariesLibrary.Models
+{
+    public class PersonModel
+    {
+        public string? Name { get; set; }
+    }
+}
