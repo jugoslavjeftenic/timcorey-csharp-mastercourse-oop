a967ee677459bda49a26e563c2d6353823bf81ad
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-22 10:43:17 +0200

    PropertyTypes

---
+++ T17_01_01_PropertyTypes/PersonModel.cs
@@ -0,0 +1,66 @@
+﻿
+namespace T17_01_01_PropertyTypes
+{
+    public class PersonModel
+    {
+        public string? FirstName { private get; set; }
+        public string? LastName { get; private set; }
+
+        private string? _password;
+
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        //public int? Age { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 0 && value < 126)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Age needs to be in a range 0-125.");
+                }
+            }
+        }
+
+        //public string SSN { get; set; } // Social Security Number 123-45-6789
+        private string? _ssn;
+
+        public string SSN
+        {
+            get
+            {
+                string output = $"***-**-{_ssn?.Substring(_ssn.Length - 4)}";
+                return output;
+            }
+            set { _ssn = value; }
+        }
+
+        public PersonModel()
+        {
+
+        }
+
+        public PersonModel(string lastName)
+        {
+            LastName = lastName;
+        }
+    }
+}
---
+++ T17_01_01_PropertyTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace T17_01_01_PropertyTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new("Corey")
+            {
+                FirstName = "Tim",
+                //LastName = "Corey",
+                Age = 30,
+                SSN = "123-45-6789"
+            };
+
+            Console.WriteLine(person.FullName);
+            Console.WriteLine(person.SSN);
+        }
+    }
+}
