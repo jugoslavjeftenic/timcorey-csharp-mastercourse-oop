0e2affcc6044be43217198fb2efd43e1437c978b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-23 14:09:16 +0200

    MiniProjectGuestBook

---
+++ T18_02_01_MiniProjectGuestBook/Program.cs
@@ -0,0 +1,53 @@
+﻿// Capture the information about each guest
+// (assumption is at least one guest and unknown maximum)
+// Info to capture: First name, last name, message to the host
+// Once done, loop through each guest and print their info
+
+using T18_02_01_MiniProjectGuestBookLibrary.Models;
+
+namespace T18_02_01_MiniProjectGuestBook
+{
+    public class Program
+    {
+        private static List<GuestModel> guests = new();
+
+        static void Main(string[] args)
+        {
+            GetGuestInformation();
+            PrintGuestInformation();
+        }
+
+        private static void GetGuestInformation()
+        {
+            string? moreGuestsComing;
+
+            do
+            {
+                GuestModel guest = new();
+
+                guest.FirstName = GetInfoFromConsole("What is your first name: ");
+                guest.LastName = GetInfoFromConsole("What is your last name: ");
+                guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
+                moreGuestsComing = GetInfoFromConsole("Are more guest coming (yes/no): ");
+
+                guests.Add(guest);
+
+                Console.Clear();
+            } while (moreGuestsComing?.ToLower() == "yes");
+        }
+
+        private static void PrintGuestInformation()
+        {
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+        }
+
+        private static string? GetInfoFromConsole(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ T18_02_01_MiniProjectGuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace T18_02_01_MiniProjectGuestBookLibrary.Models
+{
+    public class GuestModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MessageToHost { get; set; }
+
+        public string GuestInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName}: {MessageToHost}";
+            }
+        }
+    }
+}
