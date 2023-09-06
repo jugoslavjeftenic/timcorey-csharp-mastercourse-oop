92694e6fff7d77c9f44e9e5262e4eac4a8063a07
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-06 12:51:45 +0200

    HomeworkAccesModifiers

---
+++ T25_01_02_HomeworkAccesModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using T25_01_02_HomeworkAccesModifiersLibrary;
+
+namespace T25_01_02_HomeworkAccesModifiers
+{
+    // Create a Class Library and a Console application.
+    // Create a public class with members that have diferent modifiers.
+    // Try each out.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.SetAge(50);
+        }
+    }
+}
---
+++ T25_01_02_HomeworkAccesModifiersLibrary/Employee.cs
@@ -0,0 +1,22 @@
+﻿
+namespace T25_01_02_HomeworkAccesModifiersLibrary
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        internal int Age { get; private set; }
+
+        private string? _name;
+
+        public string? Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+    }
+}
