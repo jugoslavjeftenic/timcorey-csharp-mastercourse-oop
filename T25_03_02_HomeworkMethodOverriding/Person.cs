334be44e952a965de6a18f9807c301085d5d1388
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-07 10:28:50 +0200

    HomeworkMethodOverriding

---
+++ T25_03_02_HomeworkMethodOverriding/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace T25_03_02_HomeworkMethodOverriding
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ T25_03_02_HomeworkMethodOverriding/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace T25_03_02_HomeworkMethodOverriding
+{
+    // Create a Person class and override the ToString method in it.
+    // Make it return the user's first and last name.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
