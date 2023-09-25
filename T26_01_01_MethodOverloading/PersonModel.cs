5e8809b182da89dc307b26cf5b49102d35d72a6a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-25 15:49:48 +0200

    MethodOverloading

---
+++ T26_01_01_MethodOverloading/PersonModel.cs
@@ -0,0 +1,46 @@
+﻿namespace T26_01_01_MethodOverloading
+{
+    public class PersonModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
+        public PersonModel() { }
+
+        public PersonModel(string? firstName, string? lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public PersonModel(string? firstName, string? lastName, string? email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public void GenerateEmail()
+        {
+            GenerateEmail(false);
+        }
+
+        public void GenerateEmail(string domain)
+        {
+            GenerateEmail(false, domain);
+        }
+
+        public void GenerateEmail(bool firstInitialMethod, string domain = "aol.com")
+        {
+            if (firstInitialMethod)
+            {
+                Email = $"{FirstName?[..1]}.{LastName}@{domain}";
+            }
+            else
+            {
+                Email = $"{FirstName}.{LastName}@{domain}";
+            }
+        }
+    }
+}
---
+++ T26_01_01_MethodOverloading/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace T26_01_01_MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel("Tim", "Corey");
+            person.GenerateEmail(true, "gmail.com");
+
+            Console.WriteLine(person.Email);
+        }
+    }
+}
