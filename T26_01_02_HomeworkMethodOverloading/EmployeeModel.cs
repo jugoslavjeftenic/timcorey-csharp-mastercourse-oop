8582c86446ca2eb4208d2dfe9204443c8e32a1e1
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-25 15:58:16 +0200

    HomeworkMethodOverloading

---
+++ T26_01_02_HomeworkMethodOverloading/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿namespace T26_01_02_HomeworkMethodOverloading
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? JobPosition { get; set; }
+
+        public EmployeeModel()
+        {
+        }
+
+        public EmployeeModel(string? firstName, string? lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public EmployeeModel(string? firstName, string? lastName, string? jobPosition) : this(firstName, lastName)
+        {
+            JobPosition = jobPosition;
+        }
+    }
+}
---
+++ T26_01_02_HomeworkMethodOverloading/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace T26_01_02_HomeworkMethodOverloading
+{
+    // Create an EmployeeModel class.
+    // Create three different constructors that take in different amounts of data.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new EmployeeModel("Bob", "Smith");
+        }
+    }
+}
