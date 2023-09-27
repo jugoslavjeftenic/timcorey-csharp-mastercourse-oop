4380569a8a5291157cf22e33837f831bb2d5a753
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-27 10:34:30 +0200

    HomeworkGenerics

---
+++ T27_01_02_HomeworkGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace T27_01_02_HomeworkGenerics
+{
+    // Create a generic method that takes in an item and calls the ToString() method and
+    // prints that value to the Console.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintToConsole(99.99f);
+        }
+
+        private static void PrintToConsole<T>(T item)
+        {
+            Console.WriteLine(item?.ToString());
+        }
+    }
+}
