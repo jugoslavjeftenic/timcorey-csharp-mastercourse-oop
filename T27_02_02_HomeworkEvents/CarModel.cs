101adb6f0fe152308d85482fa3c6df6cefd8bc69
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-09-28 08:48:38 +0200

    HomeworkEvents

---
+++ T27_02_02_HomeworkEvents/CarModel.cs
@@ -0,0 +1,30 @@
+﻿
+namespace T27_02_02_HomeworkEvents
+{
+    public class CarModel
+    {
+        public event EventHandler<string>? CarDashboard;
+
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public bool IsRunning { get; set; } = false;
+
+        public CarModel(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public void StartEngine()
+        {
+            IsRunning = true;
+            CarDashboard?.Invoke(this, $"{Brand} {Model} engine is on.");
+        }
+
+        public void StopEngine()
+        {
+            IsRunning = false;
+            CarDashboard?.Invoke(this, $"{Brand} {Model} engine is off.");
+        }
+    }
+}
---
+++ T27_02_02_HomeworkEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace T27_02_02_HomeworkEvents
+{
+    // Create an event in a class and then consume that class.
+    // Attach a listener to the event and have it write to the console.
+
+    internal class Program
+    {
+        static void Main()
+        {
+            CarModel car = new("Peugeot", "208");
+            car.CarDashboard += Car_CarDashboard;
+            car.StartEngine();
+            Thread.Sleep(1000);
+            car.StopEngine();
+        }
+
+        private static void Car_CarDashboard(object? sender, string e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
